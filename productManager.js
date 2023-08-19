531e0f6bc7c59a472c8f86bbc2e7e64a65cbb067
brunochamus <brunochamus@hotmail.com>
2023-08-19 12:52:07 -0300

    tercer entrega server

--- productManager.js
+++ productManager.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
+import {existsSync} from 'node:fs'
+import { readFile, writeFile } from 'node:fs/promises';
 
 class ProductManager {
 
     products;
-    static id = 0;
+    static id = 1;
 
     constructor() {
         this.products = [];
@@ -21,18 +22,18 @@ class ProductManager {
         };
 
         try {
-            if (!fs.existsSync(this.path)) {
+            if (!existsSync(this.path)) {
                 const newList = [];
                 newList.push(newProduct)
 
-                await fs.promises.writeFile(this.path, JSON.stringify(newList, null, '\t'))
+                await writeFile(this.path, JSON.stringify(newList, null, '\t'))
 
             } else {
                 const repeatCode = this.products.some(e => e.code == newProduct.code)
                 repeatCode == true ? console.log("El codigo esta repetido") : this.products.push(newProduct)
                 const contentObj = await this.getProducts();
                 contentObj.push(newProduct);
-                await fs.promises.writeFile(this.path, JSON.stringify(contentObj, null, '\t'));
+                await writeFile(this.path, JSON.stringify(contentObj, null, '\t'));
 
             }
         } catch(error) {
@@ -43,12 +44,12 @@ class ProductManager {
     async deleteProducts(id) {
         const product = await this.getProducts()
         const productNoId = product.filter((producto) => producto.id !== id)
-        await fs.promises.writeFile(this.path, JSON.stringify(productNoId, null, '\t'));
+        await writeFile(this.path, JSON.stringify(productNoId, null, '\t'));
     }
 
 
     async getProducts() {
-        const content = await fs.promises.readFile(this.path, 'utf-8')
+        const content = await readFile(this.path, 'utf-8')
         const contentObj = JSON.parse(content)
         return contentObj;
     }
@@ -64,19 +65,27 @@ class ProductManager {
         let i = data.findIndex(e => e.id === id)
         product.id = id
         data.splice(i,1,product)
-        await fs.promises.writeFile(this.path, JSON.stringify(data))
+        await writeFile(this.path, JSON.stringify(data))
 
     }
 }
 
+/*
 const funcionAsincrona = async () => {
     const productManager = new ProductManager()
-    await productManager.addProduct("producto 1", "este es un productazo", 100, "Sin imagen", "kdf854", 8);
-    await productManager.addProduct("producto 2", "este es un producto", 1500, "Sin imagen", "kjf9", 8);
-    await productManager.addProduct("producto 3", "no sabemos que es", 10, "Sin imagen", "sdf54", 8);
-    await productManager.addProduct("producto 4", "se come", 300, "Sin imagen", "k854", 8);
-    await productManager.updateProducts(2,{"title": "producto nuevo","description": "soy nuevo","price": 98,"thumbnail": "Sin imagen","code": "lp89","stock": 8,"id": 4})
-    await productManager.deleteProducts(3);
+    await productManager.addProduct("producto 1", "cubiertos", 100, "Sin imagen", "abc123", 8);
+    await productManager.addProduct("producto 2", "vasos", 500, "Sin imagen", "abc456", 8);
+    await productManager.addProduct("producto 3", "platos", 120, "Sin imagen", "abc789", 8);
+    await productManager.addProduct("producto 4", "ollas", 1300, "Sin imagen", "def123", 8);
+    await productManager.addProduct("producto 5", "jarra", 200, "Sin imagen", "def456", 8);
+    await productManager.addProduct("producto 6", "mantel", 50, "Sin imagen", "def789", 8);
+    await productManager.addProduct("producto 7", "cuchilla", 300, "Sin imagen", "ghi123", 8);
+    await productManager.addProduct("producto 8", "microondas", 3000, "Sin imagen", "ghi456", 8);
+    await productManager.addProduct("producto 9", "cocina", 4000, "Sin imagen", "k854", 8);
+    await productManager.addProduct("producto 10", "heladera", 7000, "Sin imagen", "ghi789", 8);
 }
 
+
 funcionAsincrona();
+*/
+export { ProductManager } ;
